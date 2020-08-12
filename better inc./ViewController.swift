f166dcbc8f0484f5733fc185a25ea47520bbce39
Pallas Lake <pallaslake@gmail.com>
2020-08-12 11:14:47 -0700

    Initial Commit

---
+++ better inc./ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  better inc.
+//
+//  Created by Apple on 8/12/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
